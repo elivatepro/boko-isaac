b6ce398311ba0fb10a3a8c3866573264ce5dc6a9
Boko Isaac <bokoisaac15@gmail.com>
2026-01-03 13:56:08 +0100

    Deploy Fix

---
+++ website/src/types/reviews.ts
@@ -0,0 +1,11 @@
+export type Review = {
+  slug: string;
+  name: string;
+  role?: string;
+  company?: string;
+  content: string;
+  rating: number;
+  avatar?: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
